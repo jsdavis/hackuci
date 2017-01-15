23a5ce316b29b549bd6e81b755234af9248c7e90
Jacob Davis <jsdavis1010@gmail.com>
2017-01-15 10:49:01 -0800

    Previous lesson callback

--- scripts/init_lesson.js
+++ scripts/init_lesson.js
@@ -10,54 +10,29 @@ function init_lesson(num) {
   setFullTips(_.merge({}, getLangTips(), getLessonTips()));
 }
 
-function generateOutput(input, output) {
-	console.log("HERE: " + output + "\n")
+function generateOutput(output) {
+	console.log("HERE: " + output + "\n");
 	var result = {};
 
-	if (lesson.lesson.evaluateCode != null) {
-		var lines = input.split("\n");
+	var expected = getExpectedResult();
+	result.correct = expected instanceof RegExp ?
+		expected.test(output) : expected === output;
 
-		// Remove empty lines
-		// match line i with regex i for all i
-		_.remove(lines, function(line) {
-			return (_.startsWith(line, '#') || line=="");
-		});
-
-		var codeEval = true;
-
-		var evalLength = lesson.lesson.evaluateCode.length;
-		for (var i = 0; i < evalLength; i++) {
-			if (!lesson.lesson.evaluateCode[i].test(lines[i])) {
-				codeEval = false;
-				break;
-			}
-		}
-		result.correct = codeEval;
-	}
-
-	if (getExpectedResult() != null) {
-		var expected = getExpectedResult();
-		result.correct = expected instanceof RegExp ?
-			expected.test(output) : expected === output; 
-	}
 	if (!result.correct) {
-
 		var errors = Object.keys(getExceptionFeedback());
 		_.remove(errors, function(err) {
 			return !_.includes(output.toLowerCase(), err.toLowerCase());
 		});
 
 		result.output = "";
-		var begEz = false;
 		errors.forEach(function(err) {
 			result.output += getExceptionFeedback()[err] + "<br>";
-			begEz = true;
 		});
 
-		if (!begEz && !_.includes(output.toLowerCase(), 'traceback')) {
+		if (!result.output && !_.includes(output.toLowerCase(), 'traceback')) {
 			result.output = "Not quite! Try again.";
 		}
-		else if(!begEz) {
+		else {
 			result.output = output;
 		}
 
@@ -72,13 +47,20 @@ function generateOutput(input, output) {
 function next_lesson() {
 	var nextLessonNum = getLessonNum() + 1;
 	if (nextLessonNum > getNumberOfLessons()) {
-		init_lesson(0);
-		console.log("Lesson changed to 0")
 		return;
 	}
 	else {
 		init_lesson(nextLessonNum - 1);
 		console.log("Lesson changed to " + nextLessonNum);
+		return;
+	}
+}
+
+function prev_lesson() {
+	var prevLessonNum = getLessonNum() - 1;
+	if (prevLessonNum > 0) {
+		init_lesson(prevLessonNum - 1);
+		console.log("Lesson changed to " + prevLessonNum);
 		return;
 	}
 }

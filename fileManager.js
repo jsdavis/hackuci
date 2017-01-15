5b1c77309e5a221fdf95edc5f99060ae1a95b68a
Nina <ninavolkmuth@gmail.com>
2017-01-14 23:27:56 -0800

    can save files

--- fileManager.js
+++ fileManager.js
@@ -1,6 +1,14 @@
-/*function saveAsFile() {
+function saveAsFile() {
 	var code = session.getDocument().getValue();
-	var file = new Blob([code], {type: "text/"});
-	var url = window.URL.createObjectUrl(file);
-	var fileName = document.getElementById("")
-}*/
+	var file = new Blob([code], {type: "text/python"});
+	var url = window.URL.createObjectURL(file);
+	var fileName = document.getElementById("inlineFormInput").value;
+
+	var dlLink = document.createElement("a");
+	dlLink.download = fileName;
+	dlLink.href = url;
+	dlLink.style.display = "none";
+	console.log(dlLink.outerHTML);
+	document.body.appendChild(dlLink);
+	dlLink.click();
+}    

eca3a91cd77957a52c95cd7f5f10384aa545c7a5
Jacob Davis <jsdavis1010@gmail.com>
2017-01-15 09:38:08 -0800

    Tooltip enhancements

--- tooltips/python.js
+++ tooltips/python.js
@@ -1,14 +1,14 @@
 var python_tips = {
 	"language" : {
-		"#" : "Every line that starts with a <b>#</b> is a comment.<hr>Comments don't execute any code, and exist for clarification",
-		"print" : "<em>print</em> is a function in Python. When it is run, everything between the parentheses <b>()</b> after print is printed out below. The space below is called the console.",
+		"#" : "Every line that starts with a <em>#</em> is a comment.<hr>Comments don't execute any code, and exist for clarification",
+		"print" : "<em>print</em> is a function in Python. When it is run, everything between the parentheses <em>()</em> after print is printed out below. The space below is called the <i>console</i>.",
     "def" : "<em>def</em> tells the interpreter that you're planning on defining a new function.<hr><em>functions</em> allow you to break code tasks into smaller pieces.",
-    "if" : "<em>if</em> works in Python much like you might expect in English:<hr><b>if</b> x == 3:<br><pre>    print('x is 3!')</pre><hr>It takes a statement that can either be true or false, and executes the nested lines if it was true.",
-    "==" : "Single '=' <em>assigns</em> a value, while a double '==' <em>compares</em> values<hr>'==' is the same as saying 'is equal to' in English"
+    "if" : "<em>if</em> works in Python much like you might expect in English:<hr><em>if</em> x == 3:<br><pre>    print('x is 3!')</pre><hr>It takes a statement that can either be true or false, and executes the nested lines if it was true.",
+    "==" : "Single <em>=</em> <i>assigns</i> a value, while a double <em>==</em> <i>compares</i> values<hr> <em>==</em> is the same as saying <i>is equal to</i> in English"
 	},
 	"lesson": {
 		"2": {
-			"x" : "This <b>x</b> is called a variable. Variables hold values that are assigned to them with the <b>=</b> operator.<hr> In this lesson, you are assigning a number for this variable <b>x</b> to hold."
+			"x" : "This <em>x</em> is called a <i>variable</i>. Variables hold values that are assigned to them with the <em>=</em> operator.<hr> In this lesson, you are assigning a number for this variable <em>x</em> to hold."
 		}
 	}
 }

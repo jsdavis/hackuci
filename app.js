134b96c96264b857a13b8356e34e417afdfe648c
Jacob Davis <jsdavis1010@gmail.com>
2017-03-06 00:29:57 -0800

    Added logging

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 "use strict";
 
+const log = require('./log');
 const express = require('express');
 const app = express();
 const req_log = require('./middleware/req-logger');
+
+const PORT = process.env.PORT || 3000;
 
 app.use(req_log);
 app.use(express.static('public'));
@@ -11,4 +14,4 @@ app.get('/', (req, res) => {
     res.sendFile('/public/index.html');
 });
 
-app.listen(3000);
+app.listen(PORT, () => log.info('Server listening on port ' + PORT));

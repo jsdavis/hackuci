b2822861e9f76d55aa0bc5c52303875c7139422a
Jacob Davis <jsdavis1010@gmail.com>
2017-03-06 18:48:17 -0800

    Added socket connection module for repl.it on the server

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  repl: {
+    token: {
+      time_created: 1488782977000,
+      msg_mac: "er3S3ee78ksHnsxTM20fUT/rDchWoCx+tT/QUrdIp/Q="
+    },
+    lang: 'python3',
+    url: 'api.repl.it'
+  }
+}
